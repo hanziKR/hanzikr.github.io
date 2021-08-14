d17349b62c915c47ee9396ebbc5942921245701c
hanziKR <hanzikrofficial@gmail.com>
2021-08-14 18:32:48 +0900

    Xml request

--- hcloud.js
+++ hcloud.js
@@ -1,9 +1,19 @@
-(window.onload = () => {
+window.onload = () => {
+    console.log("hi");
     const listDiv = document.getElementById("list");
-    const list = ["asdf.txt", "fdsa.png"];
-    for (let i = 0; i < list; i++) {
-        let content = document.createElement("p");
-        content.textContent = list[i];
-        listDiv.appendChild(content);
+    
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            console.log(xmlHttp.responseText);
+            const list = ["asdf.txt", "fdsa.png", "fff.gif"];
+            for (let i = 0; i < list.length; i++) {
+                let content = document.createElement("p");
+                content.textContent = list[i];
+                listDiv.appendChild(content);
+            }
+        }
     }
-})();
+    xmlHttp.open("GET", "https://hanzikr.kro.kr", true);
+    xmlHttp.send();
+};
