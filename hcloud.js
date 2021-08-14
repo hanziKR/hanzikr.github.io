c515a7d35f27fc0a9a78ab60639c6910faa5c1df
hanziKR <hanzikrofficial@gmail.com>
2021-08-14 20:33:58 +0900

    My fourth web

--- hcloud.js
+++ hcloud.js
@@ -1,19 +1,31 @@
+const Hcloud = (() => {
+    const fileDownload = (fname) => {
+        window.open("https://hanzikr.kro.kr/hcloud/down?name=" + fname, '_blank');
+    }
+    return {
+        fileDownload : fileDownload
+    };
+})();
+
 window.onload = () => {
-    console.log("hi");
-    const listDiv = document.getElementById("list");
-    
     const xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = () => { 
+    xmlHttp.onload = () => { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            console.log(xmlHttp.responseText);
-            const list = ["asdf.txt", "fdsa.png", "fff.gif"];
+            const listDiv = document.getElementById("list");
+            const list = xmlHttp.responseText.split("/");
             for (let i = 0; i < list.length; i++) {
-                let content = document.createElement("p");
-                content.textContent = list[i];
-                listDiv.appendChild(content);
+                const div = document.createElement("div");
+                const content = document.createElement("a");
+                const fname = list[i];
+
+                content.textContent = fname;
+                content.setAttribute("onclick", 'Hcloud.fileDownload("' + fname + '")');
+                
+                div.appendChild(content);
+                listDiv.appendChild(div);
             }
         }
-    }
-    xmlHttp.open("GET", "https://hanzikr.kro.kr", true);
+    };
+    xmlHttp.open("GET", "https://hanzikr.kro.kr/hcloud/list", true);
     xmlHttp.send();
 };
