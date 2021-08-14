2c30f80bc3c0303a9f5e2089160a0f54eb8de7b4
hanziKR <hanzikrofficial@gmail.com>
2021-08-14 17:28:09 +0900

    Remake

---
+++ filter.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    Gcookie.loadCookieArray();
+    
+    if (Gcookie.findCookie("bfilter") == "1") {
+        Btoggle.setMode(true);
+    }
+
+    const btoggle = document.getElementById("btoggle");
+    btoggle.checked = Btoggle.getMode();
+};
---
+++ gCookie.js
@@ -0,0 +1,25 @@
+const Gcookie = (() => {
+    let cookieArray = []; 
+
+    const getCookieArray = () => {
+        return cookieArray;
+    }
+    const loadCookieArray = () => {
+        cookieArray = document.cookie.split(";");
+    }
+    const findCookie = (key) => {
+        const _key = key + "=";
+        for (let i = 0; i < cookieArray.length; i++) {
+            if (cookieArray[i].indexOf(_key) == 0) return cookieArray[i].slice(_key.length, cookieArray[i].length);
+        }
+    }
+    const setCookie = (key, data) => {
+        document.cookie = key + "=" + encodeURIComponent(data) + ";expires=Fri, 31 Dec 9999 00:00:00 UTC;domain=" + ".hanzikr.ml";
+    }
+    return {
+        getCookieArray : getCookieArray,
+        loadCookieArray : loadCookieArray,
+        findCookie : findCookie,
+        setCookie : setCookie
+    };
+})();
