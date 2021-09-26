d503d6bb2b6db7cb0a517a5ad37af86a470ba008
hanziKR <hanzikrofficial@gmail.com>
2021-09-26 12:33:20 +0900

    new

--- js/login.js
+++ js/login.js
@@ -29,46 +29,44 @@ window.addEventListener("DOMContentLoaded", () => {
         const xmlHttp = new XMLHttpRequest();
         xmlHttp.onload = () => {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                const response = JSON.parse(xmlHttp.response);
-                if (!response.success) {
-                    grecaptcha.reset();
-                    switch (response.msg) {
-                        case "iorp": {
-                            message.textContent = "id나 password가 잘못되었습니다";
-                            break;
-                        }
-                        case "recaptcha": {
-                            message.textContent = "로봇이 아님을 증명하지 못했습니다";
-                            break;
-                        }
-                        default: {
-                            message.textContent = "알 수 없는 오류가 발생했습니다";
-                            break;
-                        }
-                    }
-                }
-                else {
-                    const expires = response.data.data.expires;
-                    Gcookie.setCookie("session", response.data.session, expires);
-                    Gcookie.setCookie("uid", response.data.data.id, expires);
+                const response = xmlHttp.response;
+                console.log(response);
+                // if (!response.success) {
+                //     // switch (response.msg) {
+                //         // case "iorp": {
+                //             // message.textContent = "id나 password가 잘못되었습니다";
+                //             // break;
+                //         // }
+                //         // case "recaptcha": {
+                //             // message.textContent = "로봇이 아님을 증명하지 못했습니다";
+                //             // break;
+                //         // }
+                //         // default: {
+                //             // message.textContent = "알 수 없는 오류가 발생했습니다";
+                //             // break;
+                //         // }
+                //     // }
+                // }
+                // else {
+                //     const expires = response.data.data.expires;
+                //     Gcookie.setCookie("session", response.data.session, expires);
+                //     Gcookie.setCookie("uid", response.data.data.id, expires);
 
-                    const url = new URL(window.location.href);
-                    const c = url.searchParams.get("c");
+                //     const url = new URL(window.location.href);
+                //     const c = url.searchParams.get("c");
 
-                    if (!c || !c.startsWith("https://hanzikr.github.io") || !c.startsWith("localhost")) {
-                        window.location.href = "/";
-                    }
-                    else {
-                        window.location.href = c;
-                    }
-                }
+                //     if (!c || !c.startsWith("https://hanzikr.github.io") || !c.startsWith("localhost")) {
+                //         window.location.href = "/";
+                //     }
+                //     else {
+                //         window.location.href = c;
+                //     }
+                // }
             }
         };
-        const recres = event.target["g-recaptcha-response"].value;
         let params = "";
-        params += "g-recaptcha-response=" + recres + "&";
         params += "id=" + id + "&";
-        params += "password=" + passwordHash;
+        params += "passwordHash=" + passwordHash;
 
         xmlHttp.open("POST", "https://hanzikr.kro.kr/login", true);
 
